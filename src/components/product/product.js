d4ab3c7298b33821f51129b4188bfe5ce9615c00
viricruz <viridiana.cruz.olivera@gmail.com>
2020-11-09 16:20:58 -0700

    Add implementation code to make tests green

--- src/components/product/product.js
+++ src/components/product/product.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import ProductName from './productName'
+import ProductImage from './productImage'
+import ProductPrice from './productPrice'
 
-const Product = ({name, type}) => {
+const Product = ({ name, type, price, imageSrc }) => {
   return (
     <div>
+      <ProductImage type={type} imageSrc={imageSrc} />
       { name !=='' ? <ProductName name={name} type={type} /> : undefined }
+      <ProductPrice price={price} />
     </div> 
   )
 }
